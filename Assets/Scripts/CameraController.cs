e2f33d0ba5446e8763134b2721f2996c097ada6d
EdwardNickless <133578443+EdwardNickless@users.noreply.github.com>
2023-08-21 23:29:35 +0100

    Testing Suspension
    - Modified suspension values for wheel colliders
      -- Needs further refinement - Car bounces way too much
    - Added center of mass Transform

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private GameObject car;
+
+    [SerializeField] private Vector3 offset;
+
+    private void Start()
+    {
+        offset = new(5.0f, 2.5f, 0.0f);
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = car.transform.position + offset;
+    }
+}
