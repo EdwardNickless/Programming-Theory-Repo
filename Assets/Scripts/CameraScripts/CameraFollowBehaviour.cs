07d94f8c6b8890c90d071745f9f6dad16f6d71aa
EdwardNickless <133578443+EdwardNickless@users.noreply.github.com>
2023-09-03 22:16:16 +0100

    Camera Switching
    - Player is now able to switch the camera between follow position, Driver position and Front Bumper position
      -- Rotation needs work
      -- Camera distance increase relative to speed in follow position needs fixing

--- Assets/Scripts/CameraScripts/CameraFollowBehaviour.cs
+++ Assets/Scripts/CameraScripts/CameraFollowBehaviour.cs
@@ -4,40 +4,58 @@ public class CameraFollowBehaviour : MonoBehaviour
 {
     [SerializeField] private AnimationCurve distanceCurve;
 
+    private CameraSwitchBehaviour switchBehaviour;
     private VehicleBehaviour vehicle;
-    private Vector3 initialPosition;
+
+    private Vector3 viewPosition;
     private Vector3 targetPosition;
     private Vector3 maxDistancePosition;
 
+    private string currentView;
+
     private void Awake()
     {
-        vehicle = GetComponentInParent<VehicleBehaviour>();
+        switchBehaviour = GetComponent<CameraSwitchBehaviour>();
+        vehicle = FindObjectOfType<VehicleBehaviour>();
     }
 
     private void Start()
     {
-        initialPosition = GetInitialPosition();
-        maxDistancePosition = initialPosition + new Vector3(0.0f, 0.0f, -2.0f);
+        currentView = switchBehaviour.ViewName;
+        viewPosition = switchBehaviour.CurrentView.GetPosition();
+        maxDistancePosition = viewPosition + new Vector3(0.0f, 0.0f, -2.0f);
     }
 
-    private Vector3 GetInitialPosition()
+    private void LateUpdate()
     {
-        return transform.localPosition;
+        FollowVehicle();
     }
 
-    private void Update()
+    private void FollowVehicle()
     {
         targetPosition = CalculateNextPosition();
+        if (currentView == "Follow View")
+        {
+            targetPosition = IncreaseFollowDistance();
+        }
+        transform.position = targetPosition;
     }
 
     private Vector3 CalculateNextPosition()
     {
-        return Vector3.Lerp(initialPosition, maxDistancePosition, distanceCurve.Evaluate(vehicle.CurrentSpeed));
+        UpdateCameraOnViewChange();
+        return vehicle.transform.position + viewPosition;
     }
 
-    private void FixedUpdate()
+    private void UpdateCameraOnViewChange()
     {
-        transform.localPosition = targetPosition;
-        transform.LookAt(vehicle.transform);
+        currentView = switchBehaviour.ViewName;
+        viewPosition = switchBehaviour.CurrentView.GetPosition();
+        transform.eulerAngles = switchBehaviour.CurrentView.GetEulerAngles();
+    }
+
+    private Vector3 IncreaseFollowDistance()
+    {
+        return Vector3.Lerp(viewPosition, maxDistancePosition, distanceCurve.Evaluate(vehicle.CurrentSpeed));
     }
 }
--- Assets/Scripts/CameraScripts/CameraSwitchBehaviour.cs
+++ Assets/Scripts/CameraScripts/CameraSwitchBehaviour.cs
@@ -1,6 +1,98 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraSwitchBehaviour : MonoBehaviour
 {
-    
+    private Camera mainCamera;
+
+    private int viewIndex;
+    private string viewName;
+
+    private Vector3 behindCarPosition;
+    private Vector3 behindCarRotation;
+
+    private Vector3 operatorViewPosition;
+    private Vector3 operatorViewRotation;
+
+    private Vector3 frontBumperPosition;
+    private Vector3 frontBumperRotation;
+
+    private List<string> cameraViewNames;
+    private Dictionary<string, CameraView> cameraViews;
+
+    public string ViewName { get { return viewName; } private set { viewName = value; } }
+
+    public CameraView CurrentView { get { return cameraViews[ViewName]; } }
+
+    private void Awake()
+    {
+        mainCamera = GetComponent<Camera>();
+    }
+
+    private void Start()
+    {
+        InitialiseViews();
+        InitialiseCameraViewNamesList();
+        InitialiseCameraViewsDictionary();
+        viewIndex = 0;
+        viewName = cameraViewNames[viewIndex];
+        SetCameraPositionAndRotation(viewIndex);
+    }
+
+    private void InitialiseViews()
+    {
+        behindCarPosition = new Vector3(0.0f, 2.0f, -4.0f);
+        behindCarRotation = new Vector3(15.0f, 0.0f, 0.0f);
+
+        operatorViewPosition = new Vector3(-0.15f, 1.25f, 1.0f);
+        operatorViewRotation = new Vector3(10.0f, 0.0f, 0.0f);
+
+        frontBumperPosition = new Vector3(0.0f, 0.5f, 2.0f);
+        frontBumperRotation = new Vector3(5.0f, 0.0f, 0.0f);
+    }
+
+    private void InitialiseCameraViewNamesList()
+    {
+        cameraViewNames = new List<string>
+        {
+            "Follow View",
+            "Operator View",
+            "Bumper View"
+        };
+    }
+
+    private void InitialiseCameraViewsDictionary()
+    {
+        cameraViews = new Dictionary<string, CameraView>
+        {
+            ["Follow View"] = new CameraView(behindCarPosition, behindCarRotation),
+            ["Operator View"] = new CameraView(operatorViewPosition, operatorViewRotation),
+            ["Bumper View"] = new CameraView(frontBumperPosition, frontBumperRotation)
+        };
+    }
+
+    private void SetCameraPositionAndRotation(int viewIndex)
+    {
+        viewName = cameraViewNames[viewIndex];
+        mainCamera.transform.localPosition = cameraViews[viewName].GetPosition();
+        mainCamera.transform.localEulerAngles = cameraViews[viewName].GetEulerAngles();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            ChangeView();
+        }
+    }
+
+    private void ChangeView()
+    {
+        viewIndex++;
+        if (viewIndex >= cameraViewNames.Count)
+        {
+            viewIndex = 0;
+        }
+        SetCameraPositionAndRotation(viewIndex);
+    }
 }
